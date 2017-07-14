f9e74a5a5198062190d6f17725941a614ba29be6
Eric Feng <evf23@cornell.edu>
2017-07-13 22:28:15 -0400

    remove debug console logs, will fix firebase issue tmrw

--- routes/utils.js
+++ routes/utils.js
@@ -10,21 +10,17 @@ router.post('/create', function(req, res, next){
 });
 
 function create(question, tags){
-  console.log("ahhh")
   //creates two refs for the database, and then the questions collection
   var root = firebase.database().ref(); 
   var qref = root.child('Questions');	
   //checks if current user is signed in
-  console.log("huh??")
   console.log(firebase.auth())
   if(firebase.auth().currentUser){
     //pushes object into the questions collection
     qref.push({text:question,tag:tags});
-    console.log("what")
     return 'success'
   }
   else {
-    console.log("rejected")
     return 'Error: User not logged in!'
   }
 }
